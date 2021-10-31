ebe81801123ad95de63ada2c399660e6ddefc1aa
MijanurMim <mjanurmim1@gmail.com>
2021-10-31 12:36:21 +0600

    foter done:

--- fellow-travellers/src/pages/Shared/Footer/Footer.js
+++ fellow-travellers/src/pages/Shared/Footer/Footer.js
@@ -1,10 +1,78 @@
+import { Grid, Typography } from "@mui/material";
+import { makeStyles } from "@mui/styles";
 import React from "react";
+// custom css
+const useStyles = makeStyles((theme) => ({
+  footer: {
+    marginTop: "400px",
+    backgroundColor: "#455a64",
+    color: "white",
+    position: "sticky",
+    top: "100%",
+    bottom: "0px",
+    width: "100%",
+    paddingTop: "40px",
+  },
+  bottomFooter: {
+    backgroundColor: "#37474f",
+
+    color: "white",
+    marginTop: "20px",
+  },
+  footerSpacing: {
+    marginTop: "400px",
+  },
+}));
 
 const Footer = () => {
+  const classes = useStyles();
   return (
-    <div>
-      <h1>This is FOOTER</h1>
-    </div>
+    <Grid
+      container
+      flexDirection={"column"}
+      color="primary"
+      className={classes.footerSpacing}
+    >
+      <Grid item container flexDirection={"column"} className={classes.footer}>
+        {/* top footer area  */}
+        <Grid item container>
+          {/* this line is for side space of the page  */}
+          <Grid item xs={false} sm={4}></Grid>
+
+          <Grid item xs={12} sm={4}>
+            <Typography variant="h3">Fellow Travellers</Typography>
+          </Grid>
+          {/* this line is for side space of the page  */}
+          <Grid item xs={false} sm={4}></Grid>
+        </Grid>
+        {/* middle foter area  */}
+        <Grid item container spacing={4}>
+          {/* this line is for side space of the page  */}
+          <Grid item xs={false} sm={1}></Grid>
+          <Grid item xs={12} sm={3}>
+            <Typography>Contact</Typography>
+            <Typography>Services</Typography>
+            <Typography>Feedback</Typography>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Typography>Packages</Typography>
+            <Typography>Members</Typography>
+            <Typography>Cost</Typography>
+          </Grid>
+          <Grid item xs={12} sm={3}>
+            <Typography>Volunteer</Typography>
+            <Typography>Destinations</Typography>
+            <Typography>Security</Typography>
+          </Grid>
+
+          {/* this line is for side space of the page  */}
+          <Grid item xs={false} sm={1}></Grid>
+        </Grid>
+        <div className={classes.bottomFooter}>
+          <small>&copy; - Fellow Travellers</small>
+        </div>
+      </Grid>
+    </Grid>
   );
 };
 
--- fellow-travellers/src/pages/Shared/Header/Header.js
+++ fellow-travellers/src/pages/Shared/Header/Header.js
@@ -1,35 +1,117 @@
-import { Button, Typography } from "@mui/material";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import LoginIcon from "@mui/icons-material/Login";
+import LogoutIcon from "@mui/icons-material/Logout";
+import { AppBar, Button, Grid, Toolbar, Typography } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import { Box } from "@mui/system";
 import React from "react";
 import { Link } from "react-router-dom";
 import useAuth from "../../../hooks/useAuth";
 
+// / custom css
+const useStyles = makeStyles(() => ({
+  headerOption: {
+    display: "flex",
+    flex: 1,
+    justifyContent: "center",
+  },
+  link: {
+    textDecoration: "none",
+  },
+
+  navbarButton: {
+    width: "200px",
+  },
+}));
+
 const Header = () => {
+  const classes = useStyles();
   const { user, logOut } = useAuth();
   return (
-    <div>
-      <h1>This is Header</h1>
-      <Link to="/home">Home</Link>
+    <Grid container>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Grid container sx={{ py: "10px" }}>
+              {/* Appbar Name and logo  */}
+              <Grid item xs={12} md={2}>
+                <Typography variant="h4">
+                  <DashboardIcon />
+                  BDNHC
+                </Typography>
+              </Grid>
 
-      <Link to="/packages">Packages</Link>
+              {/* appbar routing area  */}
+              <Grid item xs={12} md={8}>
+                <Link to="/home" className={classes.link}>
+                  <Button
+                    variant="contained"
+                    sx={{ mx: 2 }}
+                    className={classes.navbarButton}
+                  >
+                    Home
+                  </Button>
+                </Link>
+                <Link to="/packages" className={classes.link}>
+                  <Button
+                    variant="contained"
+                    sx={{ mx: 2 }}
+                    className={classes.navbarButton}
+                  >
+                    Packages
+                  </Button>
+                </Link>
+                <Link to="/myBookings" className={classes.link}>
+                  <Button
+                    variant="contained"
+                    sx={{ mx: 2 }}
+                    className={classes.navbarButton}
+                  >
+                    My Bookings
+                  </Button>
+                </Link>
+                <Link to="/totalBookings" className={classes.link}>
+                  <Button
+                    variant="contained"
+                    sx={{ mx: 2 }}
+                    className={classes.navbarButton}
+                  >
+                    Total Bookings
+                  </Button>
+                </Link>
+                <Link to="/admin" className={classes.link}>
+                  <Button
+                    variant="contained"
+                    sx={{ mx: 2 }}
+                    className={classes.navbarButton}
+                  >
+                    Admin
+                  </Button>
+                </Link>
+              </Grid>
 
-      <Link to="/myBookings">My Bookings</Link>
-      <Link to="/totalBookings">Total Bookings</Link>
-      <Link to="/admin">Admin</Link>
-
-      {/* Login / Logout area  */}
-      <Typography>{user?.email}</Typography>
-      {user?.email ? (
-        <Button onClick={logOut} variant="contained">
-          Logout
-        </Button>
-      ) : (
-        <Link to="/login">
-          <Button variant="contained" sx={{ mx: 2 }}>
-            Log in
-          </Button>
-        </Link>
-      )}
-    </div>
+              {/* appbar login area  */}
+              <Grid item xs={12} md={2}>
+                {/* show user name dynamicly  */}
+                <Typography>{user?.email}</Typography>
+                {/* login button and logout  */}
+                {user?.email ? (
+                  <Button onClick={logOut} variant="contained" sx={{ mx: 2 }}>
+                    <LogoutIcon></LogoutIcon>
+                  </Button>
+                ) : (
+                  <Link to="/login">
+                    <Button variant="contained" sx={{ mx: 2 }}>
+                      <LoginIcon></LoginIcon>
+                    </Button>
+                  </Link>
+                )}
+              </Grid>
+            </Grid>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </Grid>
   );
 };
 
